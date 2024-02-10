d381b2aea4ef9ecd74a944379a7a30c6dd9fd545
motumag <motumagishu27@gmail.com>
2024-02-10 22:23:38 +0300

    activemq with spring boot

---
+++ src/main/java/com/motuma/activemqtestapp/ActiveMqTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.motuma.activemqtestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveMqTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveMqTestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/motuma/activemqtestapp/ActiveMqTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motuma.activemqtestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveMqTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
